5cf82c8d6d5505725c85ca9b1f2669f5ff992d3a
0x283A <panacea.alex@gmail.com>
2020-04-02 22:42:11 +0200

    Added missing file, finalized home-page.

---
+++ src/web/utils/FormatDateTime.ts
@@ -0,0 +1,22 @@
+// tslint:disable: no-magic-numbers
+
+import { raw } from "express";
+
+export class FormatDateTime {
+    public static epochToDate(epochSeconds: number): string {
+        return new Date(epochSeconds * 1000).toLocaleTimeString();
+    }
+
+    public static secondsToTime(rawSeconds: number): string {
+        const hours = Math.floor(rawSeconds / 3600);
+        rawSeconds %= 3600;
+        const minutes: number = Math.floor(rawSeconds / 60);
+        const seconds: number = rawSeconds % 60;
+
+        return (`${hours ? `${hours}:` : ""}${minutes ? `${this.prependZero(minutes)}:` : ""}${this.prependZero(seconds)}`);
+    }
+
+    private static prependZero(int: number): string {
+        return (int < 9) ? `0${int}` : `${int}`;
+    }
+}
